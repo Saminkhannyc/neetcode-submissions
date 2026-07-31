2c310d4a2bea34a13dd92f0ece62c2a23a162e32
Samin Khan <96319964+Saminkhannyc@users.noreply.github.com>
2026-07-30 23:12:00 -0400

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+
+print(type(10))
+variable = 10.2
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hellow World"
+print(type(variable))
+variable = [1,2, 3]
+print(type(variable))
