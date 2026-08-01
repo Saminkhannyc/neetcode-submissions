eae7e8c143cbb9c91b37c4bff297ec8988a559f4
Samin Khan <96319964+Saminkhannyc@users.noreply.github.com>
2026-07-31 21:17:18 -0400

    Add: python-read-input-practice - submission-1

---
+++ Python For Beginners/python-read-input-practice/submission-1.py
@@ -0,0 +1,12 @@
+def add_two_numbers() -> int:
+    line = input()
+    list_of_strings = line.split(",")
+    return int(list_of_strings[0]) + int(list_of_strings[1])
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
